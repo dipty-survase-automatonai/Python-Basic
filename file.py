ec39b22559fd36e9adc423b50cbf2faf6b715ff6
diptys <48465126+diptys@users.noreply.github.com>
2019-05-28 09:59:33 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 import io
 x=input("enter file name:")
-fd=io.FileIO(x)
+#fd=io.FileIO(x)
 fd=open(x)
 line=fd.readline()
 while line!=b'':
-	
 	print(line)
 	line=fd.readline()
 
